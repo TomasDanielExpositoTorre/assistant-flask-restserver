1077370ff35843a417e229e10389005095b00f4d
Tomas <tomas.exposito@estudiante.uam.es>
2025-03-08 02:01:54 +0100

    chore(flask-rest): removed print functions

--- flask-rest/api.py
+++ flask-rest/api.py
@@ -21,28 +21,29 @@ def get_devices():
 
         # RGB color mode
         if 'hs' in attrs['supported_color_modes']:
-            dev['rgb'] = attrs['rgb_color'] if attrs['rgb_color'] is not None else [0,0,0] 
-        
+            dev['rgb'] = attrs['rgb_color'] if attrs['rgb_color'] is not None else [
+                0, 0, 0]
+
         # Temperature color mode
         if 'color_temp' in attrs['supported_color_modes']:
             dev['attributes']['Temperature'] = [
                 attrs['min_color_temp_kelvin'],
                 attrs['max_color_temp_kelvin'],
-                attrs['color_temp_kelvin'] if attrs['color_temp_kelvin'] is not None else attrs['min_color_temp_kelvin']  
+                attrs['color_temp_kelvin'] if attrs['color_temp_kelvin'] is not None else attrs['min_color_temp_kelvin']
             ]
 
         data.append(dev)
-    print(data[2])
     return jsonify(data)
+
 
 @app.route('/devices', methods=['POST'])
 def post_devices():
     # Convert byte string to a regular string
-    data : dict = json.loads(request.data)
+    data: dict = json.loads(request.data)
     mapping = {
-        "Brightness" : "brightness",
-        "Temperature" : "kelvin",
-        "rgb" : "rgb_color",
+        "Brightness": "brightness",
+        "Temperature": "kelvin",
+        "rgb": "rgb_color",
         'entity_id': 'entity_id'
     }
     off = data.pop('off')
@@ -54,10 +55,9 @@ def post_devices():
 
     if off == False:
         req = {mapping[key]: value for key, value in data.items()}
-        print(req)
         color = req.get('rgb_color', False)
         temp = req.get('kelvin', False)
-        
+
         # Remove non-changing values when clashing but prioritize RGB
         if color:
             if (temp == l['min_color_temp_kelvin'] or temp == l['color_temp_kelvin']):
@@ -66,13 +66,15 @@ def post_devices():
                 req.pop('rgb_color')
             else:
                 req.pop('kelvin')
-            print(req)
-        requests.post(f"https://danubio.ii.uam.es/api/services/light/turn_on", headers=headers, json=req)
+        requests.post(
+            f"https://danubio.ii.uam.es/api/services/light/turn_on", headers=headers, json=req)
     else:
         req = {'entity_id': data['entity_id']}
-        requests.post(f"https://danubio.ii.uam.es/api/services/light/turn_off", headers=headers, json=req)
+        requests.post(
+            f"https://danubio.ii.uam.es/api/services/light/turn_off", headers=headers, json=req)
 
     return jsonify("Hola reloj!")
+
 
 def fetch_devices():
     url = 'https://danubio.ii.uam.es/api/states'
@@ -83,6 +85,7 @@ def fetch_devices():
     response = requests.get(url, headers=headers)
     return json.loads(response.text)
 
+
 def fetch_light(id):
     url = f'https://danubio.ii.uam.es/api/states/{id}'
     headers = {
@@ -92,7 +95,9 @@ def fetch_light(id):
     response = requests.get(url, headers=headers)
     return json.loads(response.text)
 
+
 def fetch_lights(devices: list):
     return [d for d in devices if 'light.' in d['entity_id']]
+
 
 app.run(host='0.0.0.0', port=8000, ssl_context=('server.crt', 'server.key'))
